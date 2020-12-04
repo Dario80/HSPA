258d5ad9751ad39385f5178f53638ccae685d38e
Dario Totaro <dario.totaro@live.com>
2020-12-04 18:19:52 +0100

    Initial Project

---
+++ Frontend/src/app/property/property-card/property-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-property-card',
+  templateUrl: './property-card.component.html',
+  styleUrls: ['./property-card.component.scss']
+})
+export class PropertyCardComponent {
+  public Property: any = {
+    "Id": 1,
+    "Name": "Birla House",
+    "Type": "House",
+    "Price": 12000
+  }
+}
